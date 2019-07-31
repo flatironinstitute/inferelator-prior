63f40e939c723ead21fb4d459de05d2acc28aa54
CJackson <christopher@cajackson.org>
2019-07-31 11:30:58 -0400

    Description stuff

---
+++ README.md
@@ -0,0 +1,7 @@
+# srrTomat0
+
+This is a set of pipelines to create expression and prior matrices for network inference. They are designed to create
+data that is compatible with the [inferelator](https://github.com/flatironinstitute/inferelator) package. In addition to
+python dependencies, this package also requires [STAR](https://github.com/alexdobin/STAR) and
+[sra-tools](http://ncbi.github.io/sra-tools/)
+
--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-install_requires = ["numpy", "pandas"]
+install_requires = ["numpy", "pandas", "HTSeq"]
 tests_require = ["coverage", "nose"]
-version = "0.1"
+version = "0.1.0"
+
+# Description from README.md
+base_dir = os.path.dirname(os.path.abspath(__file__))
+long_description = "\n\n".join([open(os.path.join(base_dir, "README.md"), "r").read()])
 
 setup(
     name = "srrTomat0",
     version = version,
     description = "SRR Pipelines: Building matrixes from raw read data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url = "https://github.com/cskokgibbs/srrTomat0",
     author = "Claudia Skok Gibbs",
     author_email = "cskokgibbs@flatironinstitute.org",
     maintainer = "Claudia Skok Gibbs",
     maintainer_email = "cskokgibbs@flatironinstitute.org",
-    packages = ["srrTomat0"],
+    packages=find_packages(include=["srrTomat0", "srrTomat0.*"], exclude=["tests", "*.tests"]),
     zip_safe = False,
     install_requires = install_requires,
     tests_require = tests_require,
