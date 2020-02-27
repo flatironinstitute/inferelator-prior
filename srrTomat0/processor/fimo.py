f68359430cb6d80ba7576f37110db68041df4d30
asistradition <asistradition@users.noreply.github.com>
2020-02-27 18:01:02 -0500

    Parallelized fimo because it was taking too long

--- srrTomat0/processor/fimo.py
+++ srrTomat0/processor/fimo.py
@@ -1,11 +1,14 @@
-import asyncio
 import os
 import io
-import concurrent.futures
+import subprocess
+import pathos
 import pandas as pd
+import tempfile
+import math
+import itertools
 
 from srrTomat0 import FIMO_EXECUTABLE_PATH
-from srrTomat0.processor.meme_parser import parse_meme_file_for_record_block
+from srrTomat0.motifs import meme
 
 BEDTOOLS_EXTRACT_SUFFIX = ".extract.fasta"
 FIMO_DATA_SUFFIX = ".fimo.tsv"
@@ -19,85 +22,101 @@ FIMO_STOP = 'stop'
 FIMO_SCORE = 'p-value'
 
 
-def fimo_scan(srr_ids, atac_bed_files, meme_file, genome_fasta_file, target_path, num_workers=5):
+def fimo_scan(atac_bed_file, meme_file, genome_fasta_file, target_path=None, num_workers=4, min_ic=None):
     """
     """
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as async_pool:
-        futures = [async_pool.map(_scan_fimo, sid, sfn, meme_file, genome_fasta_file, target_path)
-                   for sid, sfn in zip(srr_ids, atac_bed_files)]
+    fasta_output = tempfile.gettempdir() if target_path is None else os.path.abspath(os.path.expanduser(target_path))
+    extracted_fasta_file = _extract_bed_sequence(atac_bed_file, genome_fasta_file, fasta_output)
 
-    futures, _ = concurrent.futures.wait(futures)
+    try:
+        if num_workers == 1:
+            return _get_motifs(extracted_fasta_file, meme_file, output_path=target_path)
+        else:
+            meme_files = _chunk_motifs(meme_file, num_workers=num_workers, min_ic=min_ic)
 
-    return list(map(lambda x: x.results(), futures))
+            def _get_chunk_motifs(chunk_file):
+                return _get_motifs(extracted_fasta_file, chunk_file)
+
+            with pathos.multiprocessing.Pool(num_workers) as pool:
+                motif_data = [data for data in pool.imap(_get_chunk_motifs, meme_files)]
+                motif_data = pd.concat(motif_data)
+
+            for file in meme_files:
+                try:
+                    os.remove(file)
+                except FileNotFoundError:
+                    pass
+
+            return motif_data
+    finally:
+        os.remove(extracted_fasta_file)
 
 
-async def _scan_fimo(id_name, atac_bed_file, meme_file, genome_fasta, output_path):
+def _extract_bed_sequence(bed_file, genome_fasta, output_path):
 
-    if atac_bed_file is None:
-        return None
-
-    extracted_fasta_file = await _extract_bed_sequence(id_name, atac_bed_file, genome_fasta, output_path)
-    motif_data = await _get_motifs(id_name, extracted_fasta_file, meme_file, output_path)
-
-    return motif_data
-
-
-async def _extract_bed_sequence(id_name, bed_file, genome_fasta, output_path):
-
-    output_file = os.path.join(output_path, str(id_name) + BEDTOOLS_EXTRACT_SUFFIX)
+    output_file = os.path.join(output_path, genome_fasta + BEDTOOLS_EXTRACT_SUFFIX)
 
     if os.path.exists(output_file):
         return output_file
 
     bedtools_command = ["bedtools", "getfasta", "-fi", genome_fasta, "-bed", bed_file, "-fo", output_file]
+    proc = subprocess.run(bedtools_command)
 
-    try:
-        process = await asyncio.create_subprocess_exec(*bedtools_command)
-        code = await process.wait()
-    except:
-        code = 1
-        raise
-    finally:
-        if int(code) != 0:
-            print("bedtools getfasta failed for {id} ({file})".format(id=id_name, file=bed_file))
-            try:
-                os.remove(output_file)
-            except FileNotFoundError:
-                pass
-            return None
+    if int(proc.returncode) != 0:
+        print("bedtools getfasta failed for {file} (cmd)".format(file=bed_file, cmd=" ".join(bedtools_command)))
+        try:
+            os.remove(output_file)
+        except FileNotFoundError:
+            pass
+        return None
 
     return output_file
 
 
-async def _get_motifs(id_name, fasta_file, meme_file, output_path):
+def _chunk_motifs(meme_file, num_workers=4, min_ic=None):
+    temp_dir = tempfile.gettempdir()
 
-    output_file = os.path.join(output_path, str(id_name) + FIMO_DATA_SUFFIX)
+    motifs = meme.read(meme_file)
 
-    if os.path.exists(output_file):
-        return output_file
+    if min_ic is not None:
+        motifs = list(itertools.compress(motifs, [m.information_content >= min_ic for m in motifs]))
 
-    fimo_command = [FIMO_EXECUTABLE_PATH, "--text", "--parse-genomic-coord", meme_file, fasta_file]
-    code = 0
+    chunk_size = math.ceil(len(motifs) / num_workers)
 
-    try:
-       process = await asyncio.create_subprocess_exec(*fimo_command,
-                                                      stdout=asyncio.subprocess.PIPE,
-                                                      stderr=asyncio.subprocess.PIPE)
-       motif_string, _ = await process.communicate()
-       code = max(code, process.returncode)
+    files = []
 
-    except:
-        code = 1
-        raise
+    for i in range(num_workers):
+        file_name = os.path.join(temp_dir, "chunk" + str(i) + ".meme")
+        meme.write(file_name, motifs[i * chunk_size:min((i+1) * chunk_size, len(motifs))])
+        files.append(file_name)
 
-    finally:
-        if int(code) != 0:
-            print("fimo motif scan failed for {id} ({file})".format(id=id_name, file=fasta_file))
-            return None
+    return files
 
-    motif_data = parse_fimo_output(io.StringIO(motif_string.decode("utf-8")))
-    motif_data.to_csv(output_file, sep="\t", index=False)
+
+def _get_motifs(fasta_file, meme_file, output_path=None):
+
+    if output_path is not None:
+        output_file = os.path.join(output_path, fasta_file + FIMO_DATA_SUFFIX)
+
+        if os.path.exists(output_file):
+            return pd.read_csv(output_file, sep="\t", index_col=None)
+    else:
+        output_file = None
+
+    fimo_command = [FIMO_EXECUTABLE_PATH, "--skip-matched-sequence", "--parse-genomic-coord", meme_file, fasta_file]
+    proc = subprocess.run(fimo_command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+    if int(proc.returncode) != 0:
+        print("fimo motif scan failed for {meme}, {fa} (cmd)".format(meme=meme_file,
+                                                                     fa=fasta_file,
+                                                                     cmd=" ".join(fimo_command)))
+
+    motif_data = parse_fimo_output(io.StringIO(proc.stdout.decode("utf-8")))
+
+    if output_file is not None:
+        motif_data.to_csv(output_file, sep="\t", index=False)
+
     return motif_data
 
 
