1c56d1fee5a97a2e624d8c5680dab43fef569fa3
asistradition <asistradition@users.noreply.github.com>
2020-12-01 17:57:04 -0500

    Messaging

--- inferelator_prior/motifs/pwm.py
+++ inferelator_prior/motifs/pwm.py
@@ -12,13 +12,21 @@ def read(pwm_file_list, info_file, background=None, direct_only=False, pwm_has_i
          transpose=False):
 
     info_df = pd.read_csv(info_file, sep="\t")
+    info_df[MOTIF_COL] = info_df[MOTIF_COL].astype(str)
+
     motifs = []
+    pwm_not_present = []
+    pwm_malformed = []
 
     if not pwm_has_idx and pwm_alphabet is None:
         raise ValueError("pwm_alphabet is required if pwm_has_idx=False")
 
     for pwm_file in pwm_file_list:
         pwm_id = os.path.splitext(os.path.basename(pwm_file))[0]
+
+        if pwm_id not in info_df[MOTIF_COL]:
+            pwm_not_present.append(pwm_id)
+            continue
 
         if direct_only:
             direct = info_df.loc[info_df[MOTIF_COL] == pwm_id, TF_STATUS_COL].str.contains("D")
@@ -36,10 +44,11 @@ def read(pwm_file_list, info_file, background=None, direct_only=False, pwm_has_i
                 pwm = pd.read_csv(pwm_file, sep="\t", index_col=0)
             else:
                 pwm = pd.read_csv(pwm_file, sep="\t", header=None)
-                
+
             pwm = pwm.T if transpose else pwm
         except pde.ParserError:
             print("Parser error on file {f}".format(f=pwm_name))
+            pwm_malformed.append(pwm_id)
             continue
 
         pwm_alphabet = pwm.columns.tolist() if pwm_has_idx else pwm_alphabet
@@ -48,8 +57,14 @@ def read(pwm_file_list, info_file, background=None, direct_only=False, pwm_has_i
         motif.probability_matrix = pwm.values
 
         if min(pwm.values.shape) == 0:
+            pwm_malformed.append(pwm_id)
             continue
 
         motifs.append(motif)
+
+    if len(pwm_not_present) > 0:
+        print("{pwm} PWM files not found in in {c} of {cf}".format(pwm=len(pwm_not_present), c=MOTIF_COL, cf=info_file))
+    if len(pwm_malformed) > 0:
+        print("{pwm} PWM files malformed and improperly parsed".format(pwm=len(pwm_malformed)))
 
     return motifs
