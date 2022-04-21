883a1a8f9a7f5ec94914a3356e24eace0445bb4b
asistradition <asistradition@users.noreply.github.com>
2022-04-21 12:48:36 -0400

    Change to MI

--- inferelator_prior/velocity/programs.py
+++ inferelator_prior/velocity/programs.py
@@ -1,269 +1,125 @@
-import tqdm
 import numpy as np
 import scanpy as sc
 import anndata as ad
-
+import itertools
 from scipy.sparse import issparse
-from scipy import linalg
 
-import sklearn.decomposition
-from sklearn.linear_model import Lasso
-from sklearn.metrics import mean_squared_error
-from sklearn.utils.fixes import delayed
+from inferelator.regression.mi import _make_array_discrete, _make_table, _calc_mi
+
 from sklearn.utils import gen_even_slices
+from joblib import Parallel, delayed, effective_n_jobs
 
-from joblib import parallel_backend as _parallel_backend
-from joblib import Parallel, effective_n_jobs
 
-# DEFAULT ALPHA SEARCH SPACE #
-# 0 to 10 (LOGSPACE <1 & INCREASING STEPS >1) #
-ALPHA_LASSO = np.concatenate((np.array([0]),
-                              np.logspace(-4, 0, 17),
-                              np.linspace(2, 10, 5)))
+def vprint(*args, verbose=False, **kwargs):
+    if verbose:
+        print(*args, **kwargs)
 
-def program_select(data, alphas=None, random_state=50, layer='X',
-                   n_components=100, normalize=True, threshold='bic',
-                   n_jobs=-1, **kwargs):
-    """
-    Calculate a sparse PCA using sklearn MiniBatchSparsePCA for a range of
-    alpha hyperparameters.
 
-    :param data: Data object
-    :type data: ad.AnnData
-    :param alphas: A 1d array of alpha parameters, defaults to None.
-        If None is passed, a default search space will be used
-    :type alphas: np.ndarray, optional
-    :param batch_size: The batch_size for MiniBatchSparsePCA, defaults to None
-    :type batch_size: int, optional
-    :param random_state: The random state for MiniBatchSparsePCA, defaults to 50
-    :type random_state: int, optional
-    :param layer: Data object layer to use, defaults to 'X'
-    :type layer: str, optional
-    :param n_components: Number of PCs to evaluate, defaults to 100
-    :type n_components: int, optional
-    :param normalize: Depth-normalize, log-transform, and scale date before PCA,
-        defaults to True
-    :type normalize: bool, optional
-    :param threshold: Select optimization threshold, defaults to 'genes'.
-        'genes' selects alpha based on retaining 90% of genes in final model
-        'mse' minimizes mean squared error against raw data
-        'bic' minimizes BIC of deviance from full PCA model
-    :type threshold: str, optional
-    :param minibatchsparsepca: Use sklearn MiniBatchSparsePCA, defaults to True
-    :type minibatchsparsepca: bool, optional
-    :param **kwargs: Additional keyword arguments for sklearn.decomposition object
-    :return: Data object with .uns['sparse_pca'], .obsm[], and .varm[] added
-    :rtype: ad.AnnData
-    """
+def program_select_mi(data, mi_bins=10, n_comps=None, n_clusters=2, normalize=True,
+                      layer="X", max_comps=100, comp_var_required=0.0025, n_jobs=-1,
+                      verbose=False, use_hvg=False):
 
     if layer == 'X':
         d = ad.AnnData(data.X.astype(float), dtype=float)
     else:
         d = ad.AnnData(data.layers[layer].astype(float), dtype=float)
 
+    d.var = data.var.copy()
+
     if issparse(d.X):
         d.X = d.X.A
 
     n, m = d.X.shape
 
-    if alphas is None:
-        alphas = ALPHA_LASSO
-    else:
-        alphas = np.sort(alphas)
-
-    a = alphas.shape[0]
-
+    # Normalize, if required
     if normalize:
-        # Mask is the same as sc.pp.filter_genes(min_cells=10)
-        _keep_gene_mask = np.sum(d.X != 0, axis=0) >= 10
-
-        sc.pp.filter_genes(d, min_cells=10)
         sc.pp.normalize_per_cell(d)
         sc.pp.log1p(d)
+        sc.pp.highly_variable_genes(d, max_mean=np.inf, min_disp=0.01)
+
+        _keep_gene_mask = d.var['highly_variable'].values
+        d._inplace_subset_var(_keep_gene_mask)
+
+        vprint(f"Normalized and kept {d.shape[1]} highly variable genes",
+               verbose=verbose)
+
+    elif use_hvg:
+        _keep_gene_mask = data.var['highly_variable'].values
+        d._inplace_subset_var(_keep_gene_mask)
+        vprint(f"Using {d.shape[1]} highly variable genes",
+               verbose=verbose)
 
     else:
         # Dummy mask
         _keep_gene_mask = np.ones(m, dtype=bool)
+        vprint(f"Skipped normalization and kept {d.shape[1]} genes",
+               verbose=verbose)
 
-    # Center means
-    m_mean = np.mean(d.X, axis=0)
-    d.X = d.X - m_mean[None, :]
+    # PCA
+    if n_comps is None:
+        sc.pp.pca(d, n_comps=max_comps)
 
-    # Calculate baseline for deviance
-    pca_obj = sklearn.decomposition.PCA(n_components=n_components)
-    d.obsm['X_pca'] = pca_obj.fit_transform(d.X)
-    d.varm['PCs'] = pca_obj.components_.T
-    d.obsm['X_from_pca'] = pca_obj.inverse_transform(d.obsm['X_pca'])
+        # Select comps explaining more than threshold
+        n_comps = np.sum(d.uns['pca']['variance_ratio'] >= comp_var_required)
+        d.obsm['X_pca'] = d.obsm['X_pca'][:, 0:n_comps]
+        d.varm['PCs'] = d.varm['PCs'][:, 0:n_comps]
 
-    # Switch order
-    d.X = np.asfortranarray(d.X)
-
-    results = {
-        'alphas': alphas,
-        'loadings': [],
-        'means': m_mean,
-        'full_model_mse': mean_squared_error(d.X, d.obsm['X_from_pca']),
-        'mse': np.full(a, fill_value=np.nan, dtype=float),
-        'mse_X': np.full(a, fill_value=np.nan, dtype=float),
-        'bic': np.full(a, fill_value=np.nan, dtype=float),
-        'bic_X': np.full(a, fill_value=np.nan, dtype=float),
-        'aic': np.full(a, fill_value=np.nan, dtype=float),
-        'aic_X': np.full(a, fill_value=np.nan, dtype=float),
-        'nnz': np.zeros(a, dtype=int),
-        'nnz_genes': np.zeros(a, dtype=int),
-        'deviance': np.full(a, fill_value=np.nan, dtype=float)
-    }
-
-    models = []
-
-    for i in tqdm.trange(a):
-
-        mbsp = ParallelLasso(n_components=n_components,
-                             n_jobs=n_jobs,
-                             alpha=alphas[i],
-                             random_state=random_state,
-                             **kwargs)
-
-        with _parallel_backend("loky", inner_max_num_threads=1):
-            _warm = None if i == 0 else results['loadings'][-1]
-            fit_proj = mbsp.fit_transform(d.X,
-                                          d.obsm['X_pca'],
-                                          warm_start=_warm)
-
-        # Append coefficients [Comps x Genes]
-        results['loadings'].append(mbsp.components_)
-
-        # SSR
-        ssr = np.sum((fit_proj - d.obsm['X_pca']) ** 2)
-        ssr_X = np.sum((pca_obj.inverse_transform(fit_proj) - d.X) ** 2)
-
-        nnz_per_gene = np.sum(mbsp.components_ != 0, axis=0)
-
-        # Calculate BIC from SSR
-        # n * log(SSR / n) + k * log(n)
-        results['bic'][i] = n * np.log(ssr / n) + np.sum(nnz_per_gene) * np.log(n)
-        results['bic_X'][i] = n * np.log(ssr_X / n) + np.sum(nnz_per_gene) * np.log(n)
-
-        results['aic'][i] = n * np.log(ssr / n) + 2 * np.sum(nnz_per_gene)
-        results['aic_X'][i] = n * np.log(ssr_X / n) + 2 * np.sum(nnz_per_gene)
-
-        # Add summary stats
-        results['mse'][i] = ssr / d.obsm['X_pca'].size
-        results['mse_X'][i] = ssr_X / d.X.size
-        results['nnz'][i] = np.sum(nnz_per_gene)
-        results['nnz_genes'][i] = np.sum(nnz_per_gene > 0)
-        results['deviance'][i] = ssr
-
-        models.append(mbsp)
-
-        if np.sum(nnz_per_gene) == 0:
-            break
-
-    # Largest Alpha w/90% of genes
-    if threshold == 'genes':
-        select_alpha = np.nanargmax(alphas[(results['nnz_genes'] / m) > 0.9])
-    elif threshold in results:
-        select_alpha = np.nanargmin(results[threshold])
     else:
-        _msg = f"threshold={threshold} is not a valid argument"
-        raise ValueError(_msg)
+        sc.pp.pca(d, n_comps=n_comps)
 
-    results['opt_alpha'] = alphas[select_alpha]
+    vprint(f"Using {n_comps} components", verbose=verbose)
 
-    output_key = layer + "_sparsepca"
+    # Rotate back to expression space
+    pca_expr = d.obsm['X_pca'] @ d.varm['PCs'].T
+    pca_expr = _make_array_discrete(pca_expr, mi_bins, axis=0)
 
-    # Pad components with zeros if some genes were filtered during normalization
-    if results['loadings'][select_alpha].shape[0] != data.shape[1]:
-        for i in range(len(results['loadings'])):
-            v_out = np.zeros((data.shape[1], n_components), dtype=float)
-            v_out[_keep_gene_mask, :] = results['loadings'][i].T
-            results['loadings'][i] = v_out
+    vprint(f"Calculating MI for {pca_expr.shape} array", verbose=verbose)
+    mutual_info = _mutual_information(pca_expr, mi_bins, n_jobs)
 
-    results['loadings'] = np.array(results['loadings'])
+    ad_mi = ad.AnnData(mutual_info, dtype=float, obs=d.var)
 
-    data.obsm[output_key] = models[select_alpha].transform(d.X)
-    data.varm[output_key] = results['loadings'][select_alpha].copy()
-    data.uns['sparse_pca'] = results
+    vprint(f"Calculating k-NN and Leiden for {ad_mi.shape} MI array", verbose=verbose)
 
-    return data
+    sc.pp.neighbors(ad_mi, metric='correlation', use_rep='X')
+    sc.tl.leiden(ad_mi, random_state=50)
 
+    vprint(f"Identified {len(ad_mi.obs['leiden'].cat.categories)} unique clusters",
+           verbose=verbose)
 
-class ParallelLasso:
-
-    alpha = 1.0
-    n_jobs = -1
-    ridge_alpha = 0.01
-
-    components_ = None
-
-    @property
-    def coef_(self):
-        return self.components_
-
-    def __init__(self, alpha=1.0, n_jobs=-1, ridge_alpha=0.01, **kwargs):
-        self.alpha = alpha
-        self.n_jobs = n_jobs
-        self.ridge_alpha = ridge_alpha
-
-    def fit(self, X, Y, warm_start=None, **kwargs):
-
-        n, m = X.shape
-        p = Y.shape[1]
-
-        coefs = np.zeros((p, m), dtype=float)
-
-        if self.alpha == 0:
-
-            coefs[:] = linalg.solve(np.dot(X.T, X),
-                                    np.dot(X.T, Y),
-                                    assume_a='sym').T
-
-        elif self.n_jobs == 1:
-            coefs[:] = _lasso(X, Y, alpha=self.alpha)
-
-        else:
-            gram = np.dot(X.T, X)
-
-            slices = list(gen_even_slices(p, effective_n_jobs(self.n_jobs)))
-
-            views = Parallel(n_jobs=self.n_jobs)(
-                delayed(_lasso)(
-                    X,
-                    Y[:, i],
-                    alpha=self.alpha,
-                    precompute=gram,
-                    warm_start=warm_start[i, :] if warm_start is not None else None
-                    **kwargs,
-                )
-                for i in slices
-            )
-
-            for i, results in zip(slices, views):
-                coefs[i, :] = results
-
-        self.components_ = coefs
-
-        return self
-
-    def fit_transform(self, X, Y, **kwargs):
-
-        self.fit(X, Y, **kwargs)
-        return self.transform(X)
-
-    def transform(self, X):
-        return X @ self.coef_.T
+    return ad_mi
 
 
-def _lasso(X, y, warm_start=None, **kwargs):
+def _mutual_information(discrete_array, bins, n_jobs):
 
-    kwargs['fit_intercept'] = False
+    m, n = discrete_array.shape
 
-    if kwargs['alpha'] <= 0.1 and 'max_iter' not in kwargs:
-        kwargs['max_iter'] = 2500
+    slices = list(gen_even_slices(n, effective_n_jobs(n_jobs)))
 
-    if warm_start is not None:
-        lasso_obj = Lasso(warm_start=True, **kwargs)
-        lasso_obj.coef_ = warm_start.copy()
-        return lasso_obj.fit(X, y).coef_
-    else:
-        return Lasso(**kwargs).fit(X, y).coef_
+    views = Parallel(n_jobs=n_jobs)(
+        delayed(_mi_slice)(
+            discrete_array,
+            discrete_array[:, i],
+            bins,
+            logtype=np.log2
+        )
+        for i in slices
+    )
+
+    mutual_info = np.empty((n, n), dtype=float)
+
+    for i, r in zip(slices, views):
+        mutual_info[:, i] = r
+
+    return mutual_info
+
+
+def _mi_slice(x, y, bins, logtype=np.log):
+
+    n1, n2 = x.shape[1], y.shape[1]
+
+    mutual_info = np.empty((n1, n2), dtype=float)
+    for i, j in itertools.product(range(n1), range(n2)):
+        mutual_info[i, j] = _calc_mi(_make_table(x[:, i], y[:, j], bins),
+                                     logtype=logtype)
+
+    return mutual_info
