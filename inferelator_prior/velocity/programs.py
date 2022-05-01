ca1806c32a1f4fffb509648f5c76cd98e7cd9fbd
asistradition <asistradition@users.noreply.github.com>
2022-05-01 16:38:12 -0400

    Remove +1

--- inferelator_prior/velocity/programs.py
+++ inferelator_prior/velocity/programs.py
@@ -110,7 +110,7 @@ def program_select_mi(data, n_programs=2, mi_bins=10, n_comps=None, normalize=Tr
         if mcv_loss_arr is None:
             mcv_loss_arr = mcv_pcs(d.layers['counts'], n=10)
 
-        n_comps = np.median(mcv_loss_arr, axis=0).argmin() + 1
+        n_comps = np.median(mcv_loss_arr, axis=0).argmin()
 
         d.obsm['X_pca'] = d.obsm['X_pca'][:, 0:n_comps]
         d.varm['PCs'] = d.varm['PCs'][:, 0:n_comps]
