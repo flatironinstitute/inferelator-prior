031e884feb338b5ba80df4017fcd5bd256367c2d
asistradition <asistradition@users.noreply.github.com>
2023-01-13 16:12:11 -0500

    v0.4.0

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 import os
 from setuptools import setup, find_packages
 
-install_requires = ["numpy", "pandas>=1.0", "HTSeq", "pybedtools", "scipy", "pathos", "scikit-learn", "tqdm"]
-tests_require = ["coverage", "nose", "pysam"]
-version = "0.3.8"
+install_requires = [
+    "numpy",
+    "pandas>=1.0",
+    "HTSeq",
+    "pybedtools",
+    "scipy",
+    "pathos",
+    "scikit-learn",
+    "tqdm",
+    "inferelator"
+]
+tests_require = ["coverage", "pytest", "pysam"]
+version = "0.4.0"
 
 # Description from README.md
 base_dir = os.path.dirname(os.path.abspath(__file__))
-long_description = "\n\n".join([open(os.path.join(base_dir, "README.md"), "r").read()])
+
+long_description = "\n\n".join(
+    [open(os.path.join(base_dir, "README.md"), "r").read()]
+)
 
 setup(
     name="inferelator_prior",
@@ -20,7 +33,9 @@ setup(
     author_email="cj59@nyu.edu",
     maintainer="Chris Jackson",
     maintainer_email="cj59@nyu.edu",
-    packages=find_packages(include=["inferelator_prior", "inferelator_prior.*"]),
+    packages=find_packages(
+        include=["inferelator_prior", "inferelator_prior.*"]
+    ),
     zip_safe=False,
     install_requires=install_requires,
     tests_require=tests_require,
